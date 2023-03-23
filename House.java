77f77228528851fa1d2549d28ae2e451cd4431ca
mneedham01 <mneedham@smith.edu>
2023-03-23 12:03:16 -0400

    edits from class

--- House.java
+++ House.java
@@ -1,12 +1,35 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+/* This is a stub for the House class */
+public class House extends Building {
+
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address,nFloors);
     System.out.println("You have built a house: 🏠");
+    this.residents =new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
+  }
+
+  public String toString(){
+    String description = super.toString();
+    description +=" There are currently "+this.residents.size()+ " residents in this house. This house";
+    if (this.hasDiningRoom){
+      description+= " has";
+    }else{
+      description+= "does not have ";
+    }
+    description+= " a dining room.";
+
+    return description;
   }
 
   public static void main(String[] args) {
-    new House();
+    House northrop= new House("Northrop","49 Elm Street", 4, true);
+    System.out.println(northrop);
   }
 
 }
+
