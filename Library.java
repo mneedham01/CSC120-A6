eeccd49d43664b87439599502ad4527de73cd942
mneedham01 <mneedham@smith.edu>
2023-03-27 19:33:28 -0400

    add checkOut and returnBook

--- Library.java
+++ Library.java
@@ -1,12 +1,64 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+/*
+ * 
+ */
+public class Library extends Building{
+
+    private Hashtable <String, Boolean> collection;
+
+    /*
+     * 
+     */
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String,Boolean>(); 
     }
+
+    /*
+     * 
+     */
+    public void addTitle(String title){
+      if(this.collection.contains(title)){
+        throw new RuntimeException(this.name+" library already owns "+title+"."); 
+      } else{
+        this.collection.put(title,true);
+        System.out.println(title+" has been added to "+this.name+" library and is ready to be checked out.");
+      }
+    }
+
+    /*
+     * 
+     */
+    public String removeTitle(String title){
+      if (!this.collection.contains(title)){
+        throw new RuntimeException(this.name+ " library does not own "+title+".");
+      }else{
+        this.collection.remove(title);
+        System.out.println(title+" has been removed from "+this.name+" library.");
+        return title;
+      }
+    }
+
+    /*
+     * 
+     */
+    public void checkOut(String title){
+      this.collection.replace(title, false);
+    }
+
+    /*
+     * 
+     */
+    public void returnBook(String title){
+      this.collection.replace(title,true);
+    }
+
   
     public static void main(String[] args) {
-      new Library();
+      Library neilson= new Library("Neilson", "7 Neilson Drive", 4);
+      neilson.addTitle("Charlotte's Web");
+      
     }
   
   }
