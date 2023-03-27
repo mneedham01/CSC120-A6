854dda28972961362a38c5d438d5870ef6d71995
mneedham01 <mneedham@smith.edu>
2023-03-27 10:37:33 -0400

    added more methods

--- House.java
+++ House.java
@@ -1,11 +1,16 @@
 import java.util.ArrayList;
 
-/* This is a stub for the House class */
+/*
+ * 
+ */
 public class House extends Building {
 
   private ArrayList<String> residents;
   private boolean hasDiningRoom;
 
+  /*
+   * 
+   */
   public House(String name, String address, int nFloors, boolean hasDiningRoom) {
     super(name, address,nFloors);
     System.out.println("You have built a house: 🏠");
@@ -13,6 +18,9 @@ public class House extends Building {
     this.hasDiningRoom = hasDiningRoom;
   }
 
+  /*
+   * 
+   */
   public String toString(){
     String description = super.toString();
     description +=" There are currently "+this.residents.size()+ " residents in this house. This house";
@@ -26,8 +34,66 @@ public class House extends Building {
     return description;
   }
 
+  /*
+   * 
+   */
+  public boolean hasDiningRoom(){
+    if (hasDiningRoom){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  /*
+   * 
+   */
+  public int nResidents(){
+    return residents.size();
+  }
+
+  /*
+   * 
+   */
+  public void moveIn(String name){
+    if (!residents.contains(name)){
+      residents.add(name);
+    } else{
+      System.out.println("put in error here?");
+    }
+  }
+
+  /*
+   * 
+   */
+  public String moveOut(String name){
+    if (residents.contains(name)){
+      residents.remove(name);
+      return name;
+    } else{
+      System.out.println("put in error here?");
+      return name;
+    }
+  }
+
+  /*
+   * 
+   */
+  public boolean isResident(String person){
+    if (residents.contains(person)){
+      return true; 
+    } else{
+      return false;
+    }
+  }
+
+
   public static void main(String[] args) {
     House northrop= new House("Northrop","49 Elm Street", 4, true);
+    System.out.println(northrop);
+    northrop.moveIn("Tessa");
+    System.out.println(northrop);
+    northrop.moveOut("Tessa");
     System.out.println(northrop);
   }
 
